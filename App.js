2ba0a90f3e1056fe6b6e2a1c538aa9490681f116
Guilherme Gaddini <ferreiragaddini@bol.com.br>
2020-07-14 14:36:57 -0300

    MegaSenaDrawer Commit

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  const [texto, setTexto] = useState('Texto Inicial');
+
+  const [numeros, setNumeros] = useState([]);
+
+  const onPressFunction = () => {
+    let teste = [];
+    while (teste.length < 6) {
+      var aleatorio = Math.floor(Math.random() * 60);
+      if (teste.indexOf(aleatorio) == -1)
+        teste.push(aleatorio);
+    }
+    setNumeros(teste);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ marginBottom: 16 }}>{numeros.map((numero) => {
+        return numero + ' - '
+      })}</Text>
+      <Button
+        title="Sortear Números"
+        onPress={() => onPressFunction()} />
     </View>
   );
 }
@@ -14,7 +31,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
